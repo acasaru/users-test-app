b8f4f88af8920743d3659ceaa54f48bbb22b2553
a@b.com <a@b.com>
2016-12-12 22:00:29 +0200

    add Users.Client

---
+++ AngularRouter/scripts/app.js
@@ -0,0 +1,102 @@
+﻿var myApp = angular.module('helloworld', ['ui.router']);
+
+angular.module('helloworld').component('hello', {
+    template: '<h3>{{$ctrl.greeting}} solar sytem!</h3>' +
+               '<button ng-click="$ctrl.toggleGreeting()">toggle greeting</button>',
+
+    controller: function () {
+        this.greeting = 'hello';
+
+        this.toggleGreeting = function () {
+            this.greeting = (this.greeting == 'hello') ? 'whats up' : 'hello'
+        }
+    }
+})
+
+angular.module('helloworld').component('people', {
+    bindings: { people: '<' },
+
+    template: '<h3>Some people:</h3>' +
+              '<ul>' +
+              '  <li ng-repeat="person in $ctrl.people">' +
+              '    <a ui-sref="person({ gogu: person.id })">' +
+              '      {{person.name}}' +
+              '    </a>' +
+              '  </li>' +
+              '</ul>' +
+              '<ui-view></ui-view>'
+})
+
+angular.module('helloworld').component('person', {
+    bindings: { person: '<' },
+    template: '<h3>A person!</h3>' +
+
+              '<div>Name: {{$ctrl.person.name}}</div>' +
+              '<div>Id: {{$ctrl.person.id}}</div>' +
+              '<button ui-sref="people">Close</button>'
+});
+
+angular.module('helloworld').service('PeopleService', function ($http) {
+    var service = {
+        getAllPeople: function () {
+            return $http.get('data/people.json').then(function (response) {
+                return response.data;
+            });
+        },
+
+        getPerson: function (personId) {
+            return service.getAllPeople().then(function (people) {
+                return people.find(function (person) {
+                    return person.id === personId;
+                })
+            })
+        }
+    };
+    return service;
+});
+
+
+myApp.config(function ($stateProvider) {
+    var helloState = {
+        name: 'hello',
+        url: '/hello',
+        component: 'hello'
+    };
+    var aboutState = {
+        name: 'about',
+        url: '/about',
+        template: '<h3>Its the UI-Router<br>Hello Solar System app!</h3>'
+    };
+    
+    var peopleState = {
+        url: '/people',
+        name: 'people',
+        component: 'people',
+        resolve: {
+            people: function (PeopleService) {
+                return PeopleService.getAllPeople();
+            }
+        }
+    }
+
+    var personState = {
+        name: 'person',
+        parent:'people',
+        url: '/{gogu}',
+        component: 'person',
+        resolve: {
+            person: function (people, $transition$) {
+                return people.find(function (person) {
+                    return person.id === $transition$.params().gogu;
+                });
+            }
+        }
+    }
+    $stateProvider.state(helloState);
+    $stateProvider.state(aboutState);
+    $stateProvider.state(peopleState);
+    $stateProvider.state(personState);
+});
+
+
+
---
+++ AngularRouter/scripts/loginApp.js
@@ -0,0 +1,204 @@
+﻿var myApp = angular.module('loginApp', ['ui.router', 'ngMessages', 'ui.bootstrap']);
+
+myApp.config(function ($stateProvider) {
+    var states = [
+    {
+        url: '/login',
+        name: 'login',
+        component: 'login'
+    },
+    {
+        name: 'users',
+        abstract: true,
+        url:'/users',
+        templateUrl: 'views/users/main.html',
+        defaultSubstate:'users.list'
+    },
+     {
+         name: 'users.list',
+         url: '/list',
+         templateUrl: 'views/users/list.html',
+         controller: 'usersCtrl',
+         controllerAs: 'usersCtrl'
+     },
+     {
+         name: "users.edit",
+         url: '/edit/{id}',
+         templateUrl: 'views/users/edit.html',
+         controller: 'editCtrl',
+         controllerAs: 'editCtrl'
+     },
+     {
+         name: "users.delete",
+         url: '/delete/{id}',
+         templateUrl: 'views/users/delete.html',
+         controller: 'deleteCtrl',
+         controllerAs: 'deleteCtrl'
+     },
+     {
+          name: 'users.create',
+          url: '/create',
+          templateUrl: 'views/users/create.html',
+          controller: 'createCtrl',
+          controllerAs: 'createCtrl',
+          resolve: {
+              roles: function (roleServices) { return roleServices.getRoles(); }
+          }
+      }
+    ];
+
+    states.forEach(function (state) {
+        $stateProvider.state(state);
+    });
+});
+
+
+
+myApp.component('login', {
+    templateUrl: 'views/login.html',
+    controller: function ($rootScope, $state) {
+        this.submit = function () {
+            $rootScope.isAuthenticated = true;
+            $rootScope.username = this.username;
+            $state.go('users.list');
+        }
+    }
+});
+
+myApp.controller('editCtrl', function ($stateParams, $rootScope, $state) {
+    var vm = this;
+    vm.id = $stateParams.id;
+    vm.user = $rootScope.users.find(function (user) {
+        return user.Id === vm.id
+    })
+
+    vm.save = function () {
+        $state.go('users.list');
+    }
+});
+
+myApp.controller('deleteCtrl', function ($uibModal, $stateParams, $rootScope, $state, $rootScope, $log) {
+    var vm = this;
+    vm.id = $stateParams.id;
+    vm.user = $rootScope.users.find(function (user) {
+        return user.Id === vm.id
+    });
+    vm.open = open;
+    vm.open();
+
+    function open() {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/users/deleteModal.html',
+            controller: 'deleteModalInstanceCtrl',
+            controllerAs: '$ctrl',
+            resolve: {
+                user: function () {
+                    return vm.user;
+                }
+            }
+        });
+
+        modalInstance.result.then(function () {
+            $state.go('users.list');
+        }, function () {
+            $state.go('users.list');
+        });
+
+        return modalInstance;
+    };   
+})
+
+myApp.controller('deleteModalInstanceCtrl', function ($uibModalInstance, $rootScope, user) {
+    var $ctrl = this;
+    $ctrl.user = user;
+
+    $ctrl.ok = function () {
+        $uibModalInstance.close('success');
+    };
+
+    $ctrl.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+})
+
+myApp.controller('usersCtrl', function (userServices, $rootScope) {
+    var vm = this;
+
+    userServices.getUsers().then(function (users) {
+        vm.users = $rootScope.users || users;
+        $rootScope.users = vm.users;
+    });
+    console.log('List ctrl');
+});
+
+myApp.controller('createCtrl', function (roles, $state, $rootScope) {
+    var vm = this;
+
+    vm.roles = roles;
+    vm.username = "";
+    vm.firstName = "";
+    vm.lastName = "";
+    vm.selectedRole = "";
+    vm.email = "";
+    vm.password = "";
+
+    vm.saveUser = function () {
+        $rootScope.users.push({
+            FirstName: vm.firstName,
+            LastName: vm.lastName,
+            UserName: vm.username
+        });
+        $state.go('users.list');
+    }
+});
+
+myApp.factory('userServices', ['$http', function ($http) {
+    var service = {
+        getUsers: function() {
+            return $http.get('data/users.json').then(
+                function (response) { return response.data; });
+
+        },
+       
+        getUser: function (id) {
+            return getUsers().then(function (users) {
+                users.find(function (user) {
+                    return user.Id === id;
+                })
+            })
+        },
+    }
+    return service;
+}]);
+
+myApp.factory('roleServices', ['$http', function($http){
+    var factory = {
+        getRoles: function(){
+            return $http.get('data/roles.json').then(function(response){return response.data;});
+        }
+    };
+
+    return factory;
+}]);
+
+
+
+
+myApp.run(function ($rootScope, $state, $location, $transitions) {
+    $rootScope.isAuthenticated = false;
+    var criteria = {
+        to: function(state) {
+            return state.name != 'login';
+        }
+    }
+    $transitions.onBefore( criteria, function(trans) {
+        var shouldLogin = $rootScope.isAuthenticated === false;
+
+        console.log(shouldLogin);
+
+        if (shouldLogin) {
+            return trans.router.stateService.target('login');
+        }
+    })
+});
+
